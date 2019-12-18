43581c80fdf8a8d3ae75e724a2027de63967626a
securesubmit-buildmaster <msmedalj@gmail.com>
2019-12-18 15:28:20 -0500

    clearn & comment

---
+++ scratch/CreateCard.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+use Omnipay\Omnipay;
+
+$gateway = Omnipay::create('GlobalPayments\Heartland');
+$gateway->setSecretApiKey('skapi_cert_MXvdAQB61V4AkyM-x3EJuY6hkEaCzaMimTWav7mVfQ');
+
+// Example form data
+$formData = [
+    'number'            => '5454545454545454', // required
+    'expiryMonth'       => '12', // required
+    'expiryYear'        => '2025', // required
+    'cvv'               => '123', // optional
+    'firstName'         => 'Tony', // optional
+    'lastName'          => 'Smedal', // optional
+    'billingAddress1'   => '1 Heartland Way', // optional
+    'billingCity'       => 'Jeffersonville', // optional
+    'billingPostcode'   => '47130', // optional
+    'billingState'      => 'IN', // optional
+    'billingCountry'    => 'USA' // optional
+];
+
+$response = $gateway->createCard(
+    [
+        'card'          => $formData
+    ]
+)->send();
+
+// Heartland doesn't use a redirect, so isSuccessful() and isDecline() are used to evaluate result and isRedirect() is not used.
+// Response.php holds all of these supported methods
+if ($response->isSuccessful()) {
+    echo $response->getCardReference();
+} elseif ($response->isDecline()) {
+    echo $response->getMessage();
+}
